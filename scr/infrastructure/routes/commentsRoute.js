b66f5290c41b906b5a723d629a7bb2f27a61cf08
Juan Camilo Blandon Lopez <jblandol77@gmail.com>
2024-05-03 18:02:13 -0500

    "ADD: Se agrega la documentación swagger para comentarios"

--- scr/infrastructure/routes/commentsRoute.js
+++ scr/infrastructure/routes/commentsRoute.js
@@ -5,6 +5,61 @@ const {createComment, searchCommentsByIdProduct} = require('../controllers/comme
 const { check } = require('express-validator');
 const { validateFields } = require('../middlewares/validateFields');
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Comment:
+ *       type: object
+ *       properties:
+ *         content: 
+ *           type: string
+ *           description: The content of the comment
+ *         rate:
+ *           type: number
+ *           description: The rating of the product (from 1 to 5)
+ *       required:
+ *         - content
+ *         - rate
+ */
+
+/**
+ * @swagger
+ * /api/v1/createComment/{id}:
+ *   post:
+ *     summary: Create a new comment for a product
+ *     tags: [Comments]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the product to comment on
+ *         schema:
+ *           type: string
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Comment'
+ *           example:
+ *             content: Excelente producto
+ *             rate: 5
+ *     responses:
+ *       200:
+ *         description: successfully created comment
+ *       400:
+ *         description: Bad request - missing or invalid parameters
+ *       401:
+ *         description: Unauthorized - Missing or invalid token
+ *       404:
+ *         description: The product to be commented on does not exist
+ *       500:
+ *         description: Something went wrong, please contact the admin
+ */
+
 
 router.post(
   '/createComment/:id', 
@@ -15,6 +70,31 @@ router.post(
   ],
   createComment
 );
+
+/**
+ * @swagger
+ * /api/v1/searchComments/{id}:
+ *   get:
+ *     summary: Get comments for a product by ID
+ *     tags: [Comments]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the product to retrieve comments for
+ *         schema:
+ *           type: string
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Comments retrieved successfully
+ *       401:
+ *         description: Unauthorized - Missing or invalid token
+ *       404:
+ *         description: The product does not exist
+ */
+
 
 router.get(
   '/searchComments/:id', 
